98d4ded3ceaa3d9e104b41ad774bc9165ebb88f6
daisy <daisy@mevry.com>
2018-08-20 12:32:23 +0200

    Data visualization with matplotlib

--- pandas-airline-safety.py
+++ pandas-airline-safety.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 print('Read data from a csv file...')
 data = pd.read_csv("airline-safety.csv")
+print(data)
 
 print("How many rows and columns are there?")
 print(data.info())
@@ -16,6 +19,51 @@ print("How many accidents are there?")
 print(data.iloc[:, 3:].sum())
 
 print("Top 10 airlines have the most accidents?")
-print(data.iloc[:, 3:].sum(axis=1).sort_values(ascending=False).head(10))
-data['totalAcc'] = sort_values(ascending=False).head(10)
+# print(data.iloc[:, 3:].sum(axis=1).sort_values(ascending=False).head(10))
+data['totalAcc'] = data.iloc[:, 2:].sum(axis=1)
 print(data[["airline", "totalAcc"]].sort_values(by="totalAcc", ascending=False).head(10))
+
+print("Data visualization...")
+data2 = data.iloc[:, 2:8].sum()
+N = 2
+ind = np.arange(N)  # the x locations for the groups
+width = 0.2      # the width of the bars
+
+fig, ax = plt.subplots()
+
+incidents = (data2[[0,3]])
+incidents_std = (0, 1)
+rects1 = ax.bar(ind, incidents, width, color='r', yerr=incidents_std)
+
+fatal_accidents = (data2[[1,4]])
+fatal_accidents_std = (3, 5)
+rects2 = ax.bar(ind + width, fatal_accidents, width, color='y', yerr=fatal_accidents_std)
+
+fatalities = (data2[[2,5]])
+fatalities_std = (3, 5)
+rects3 = ax.bar(ind + width *2, fatalities, width, color='g', yerr=fatalities_std)
+
+# add some text for labels, title and axes ticks
+ax.set_ylabel('Amount')
+ax.set_title('Airline Safety')
+ax.set_xticks(ind + width)
+ax.set_xticklabels(('1985-1999', '2000-2014'))
+
+ax.legend((rects1[0], rects2[0], rects3[0]), ('Incidents', 'Fatal-Accidents', 'Fatalities'))
+
+
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+plt.savefig('airline-safety.png')
+plt.show()
